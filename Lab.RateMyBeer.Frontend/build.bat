68837fe65c2dcec7279e82e62c95afe6dd3cc20f
Tobias Richling <RichlinT@apetito.com>
2021-06-11 09:09:21 +0200

    first working k8s deployment

--- Lab.RateMyBeer.Frontend/build.bat
+++ Lab.RateMyBeer.Frontend/build.bat
@@ -1,2 +1,4 @@
 docker build -f Dockerfile -t ratemybeercontainer.azurecr.io/frontend:dev .\..
 docker push ratemybeercontainer.azurecr.io/frontend:dev
+
+REM kubectl rollout restart deployment/lab-ratemybeer-frontend
---
+++ Lab.RateMyBeer.Frontend/entrypoint.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+set -eu
+
+envsubst '${BLAZOR_ENVIRONMENT}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+
+exec "$@"
