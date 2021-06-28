8c26925b066a21a2825eae10af314802f5f143a8
Tobias Richling <RichlinT@apetito.com>
2021-06-28 17:36:20 +0200

    start working on creating a walkthrough

--- Lab.RateMyBeer.Checkins/build.bat
+++ Lab.RateMyBeer.Checkins/build.bat
@@ -1,5 +1,6 @@
-REM az acr login --name ratemybeercontainer
+cd %~dp0
+
 docker build -f Dockerfile -t ratemybeercontainer.azurecr.io/checkinsapi:dev .\..
 docker push ratemybeercontainer.azurecr.io/checkinsapi:dev
 
-kubectl rollout restart deployment/lab-ratemybeer-checkins-api
+REM kubectl rollout restart deployment/lab-ratemybeer-checkins-api
--- Lab.RateMyBeer.Frontend/build.bat
+++ Lab.RateMyBeer.Frontend/build.bat
@@ -1,5 +1,6 @@
-REM az acr login --name ratemybeercontainer
+cd %~dp0
+
 docker build -f Dockerfile -t ratemybeercontainer.azurecr.io/frontend:dev .\..
 docker push ratemybeercontainer.azurecr.io/frontend:dev
 
-kubectl rollout restart deployment/lab-ratemybeer-frontend
+REM kubectl rollout restart deployment/lab-ratemybeer-frontend
